856d283438cfe92895a4b6ea6e27cd40392edec0
Acash16 <akash.connect60@gmail.com>
2026-05-17 02:12:42 +0530

    Fixed dashboard template issue

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ def dashboard():
     conn.close()
 
     return render_template(
-        "dashboard.html",
+        "Dashboard.html",
         user=user_data,  # Passed for the profile form!
         auctions=auctions,
         bids=bids,
